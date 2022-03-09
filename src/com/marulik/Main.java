f2b8c79ae4224c476dbd2b165846ca0277256f14
Marulik <3417991@mail.ru>
2022-03-09 09:00:51 +0300

    adding all files

---
+++ src/com/marulik/Main.java
@@ -0,0 +1,10 @@
+package com.marulik;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// в терминале пишем git init
+       // создаем в корневой папке файл .gitignore, указываю, игнорируемые файлы и папки
+        // в терминале пишем git add.
+    }
+}
